1f07462f0cd24adc4ec83d8ab2346c4bbb763331
Monique-K <mkhoury87@hotmail.com>
2018-11-28 11:45:46 -0500

    added dragons boards and changed bg colour to state variable

--- src/home/carousels/dragons/dragons304Carousel.jsx
+++ src/home/carousels/dragons/dragons304Carousel.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
 
-const t1 = require('../../../images/dragons/Dragons_Seq304_Thumbs/DRGN_Page01.jpg'); 
-const t2 = require('../../../images/dragons/Dragons_Seq304_Thumbs/DRGN_Page02.jpg'); 
-const t3 = require('../../../images/dragons/Dragons_Seq304_Thumbs/DRGN_Page03.jpg'); 
+const t1 = 'https://i.imgur.com/GlLPQDB.jpg'; 
+const t2 = 'https://i.imgur.com/SCtMZpS.jpg'; 
+const t3 = 'https://i.imgur.com/JwYOWQ7.jpg'; 
 
 const dragonsSeq304Imgs = [t1, t2, t3]
 
@@ -17,7 +17,7 @@ export default class Dragons304Carousel extends React.Component {
       width = "300"
       easing = "easeOutCirc"
       speed = {2000}
-      autoplayInterval = {1000}
+      autoplayInterval = {3000}
       >
       {dragonsSeq304Imgs.map(img => {
         return <img src={img} alt="Dragons sequence 304 thumbnails" className="thumbs" />
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 // import GrannyCarousel from './carousels/willoughbys/meet-nanna/grannyCarousel';
 import BreakfastCarousel from './carousels/willoughbys/breakfast/breakfast-carousel';
-// import Dragons304Carousel from './carousels/dragons/dragons304Carousel';
+import Dragons304Carousel from './carousels/dragons/dragons304Carousel';
 // import OpenSeasonCarousel from './carousels/openSeason/openSeasonCarousel';
-import MelanoffStoryCarousel from './carousels/willoughbys/melanoff/melanoffCarousel';
+import MelanoffStoryCarousel from './carousels/willoughbys/melanoffStory/melanoffStoryCarousel';
 // import TenLivesConfessionCarousel from './carousels/tenLives/tenLivesConfession';
 // import OpenSeason1 from './videoPlayers/openSeason';
 import Resume from '../resume/resume';
 
-const hero1 = require('../images/MainPage01.jpg')
-const hero2 = require('../images/MainPage02.jpg')
-const hero3 = require('../images/MainPage03.jpg')
-const hero4 = require('../images/MainPage04.jpg')
-const hero5 = require('../images/MainPage05.jpg')
+const hero1 = require('../images/homePage/MainPage01.jpg')
+const hero2 = require('../images/homePage/MainPage02.jpg')
+const hero3 = require('../images/homePage/MainPage03.jpg')
+const hero4 = require('../images/homePage/MainPage04.jpg')
+const hero5 = require('../images/homePage/MainPage05.jpg')
 
 const WSticky = require('../images/sticky-notes/StickyNote01.jpg');
 const OSticky = require('../images/sticky-notes/StickyNote04.jpg');
@@ -27,7 +27,8 @@ class Home extends Component {
   constructor() {
     super()
     this.state = {
-      carousel: "hero"
+      carousel: "hero",
+      backgroundColour: "gray"
     }
   }
 
@@ -44,8 +45,8 @@ class Home extends Component {
       //   return (<GrannyCarousel className="hero-carousel" />);
       case "breakfast":
         return (<BreakfastCarousel className="hero-carousel" />);
-      // case "dragons":
-      //     return (<Dragons304Carousel className="hero-carousel" />);
+      case "dragons":
+          return (<Dragons304Carousel className="hero-carousel" />);
       // case "openSeason":
       //     return (<OpenSeasonCarousel className="hero-carousel" />);
       case "melanoff":
@@ -72,34 +73,34 @@ class Home extends Component {
     return heroImg;
   }
   showHeroImg() {
-    this.setState({carousel: "hero"})
+    this.setState({carousel: "hero", backgroundColour: "gray"})
   }
   // showGrannyCarousel() {
   //   this.setState({carousel: "granny"})
   // }
   showBreakfastCarousel() {
-    this.setState({carousel: "breakfast"})
+    this.setState({carousel: "breakfast", backgroundColour: "gray"})
   }
   showMelanoffCarousel() {
-    this.setState({carousel: "melanoff"})
+    this.setState({carousel: "melanoff", backgroundColour: "gray"})
   }
   showTenLivesCarousel() {
-    this.setState({carousel: "tenLives"})
+    this.setState({carousel: "tenLives", backgroundColour: "gray"})
   }
   showTransformersCarousel() {
-    this.setState({carousel: "transformers"})
+    this.setState({carousel: "transformers", backgroundColour: "gray"})
   }
   showDragonsCarousel() {
-    this.setState({carousel: "dragons"})
+    this.setState({carousel: "dragons", backgroundColour: "gray"})
   }
   // showOpenSeasonCarousel() {
   //   this.setState({carousel: "openSeason"})
   // }
   showTenLivesConfession() {
-    this.setState({carousel: "tenLivesConfession"})
+    this.setState({carousel: "tenLivesConfession", backgroundColour: "gray"})
   }
   handleResumeClick() {
-    this.setState({carousel: "resume"})
+    this.setState({carousel: "resume", backgroundColour: "gray"})
   }
   
 
@@ -132,14 +133,21 @@ class Home extends Component {
           <div className="title-container">
             <div>
               <img 
-                src={require("../images/headerImg.jpg")} 
+                src={require("../images/homePage/headerImg.jpg")} 
                 alt="Mark Stanleigh header" 
                 className="title-image"
               />
             </div>
           </div>
-            <div className="home-hero-img">
+            <div className={`home-hero-img ${this.state.backgroundColour === "gray" ? "gray" : "white"}`}>
               {this.renderHeroComponent()}
+            </div>  
+            <div className="logos-container">
+              <img 
+                src={require("../images/homePage/CompanyLogos.jpg")} 
+                alt="Logos" 
+                className="logos"
+              />
             </div>
           </div>
         </div>
